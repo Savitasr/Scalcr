fa10db1dc666fe091f2ade5dc17409d117fd8a28
Savita.R.S <100186525+Savitasr@users.noreply.github.com>
2022-11-14 19:10:09 +0530

    Update test_handler.py

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -3,7 +3,7 @@ from hello_world import app
 
 class calculator(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(app.add(5,3),8)
+        self.assertEqual(app.add(5,2),7)
     def test_sub(self):
         self.assertEqual(app.sub(5,3),2)
     def test_mul(self):
